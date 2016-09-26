3a286d5d4e9c697f1e53aa15180f083513236cdb
andres.marulanda <andres.marulanda@l>
2016-09-26 14:15:22 -0500

    Se añade funcionalidad de calculo de funciones basicas.

---
+++ calculadora/src/java/co/edu/poli/ingenieria/sistemas/operaciones/EjecutarOperacion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.poli.ingenieria.sistemas.operaciones;
+
+/**
+ *
+ * @author andres.marulanda
+ */
+public interface EjecutarOperacion {
+
+    double ejecutarOperacion(String operacion) throws  Exception;
+    
+    String resolverOperaciones(String operacion) throws  Exception;
+}
---
+++ calculadora/src/java/co/edu/poli/ingenieria/sistemas/operaciones/impl/EjecutarOperacionImpl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.poli.ingenieria.sistemas.operaciones.impl;
+
+import co.edu.poli.ingenieria.sistemas.operaciones.EjecutarOperacion;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+
+/**
+ *
+ * @author andres.marulanda
+ */
+public class EjecutarOperacionImpl implements  EjecutarOperacion {
+
+    @Override
+    public double ejecutarOperacion(String operacion) throws  Exception{
+        double d = 0;
+        try {
+            
+        } catch (Exception e) {
+            System.out.println("Error ejecutando operaciones : "+ e.getMessage());
+        }
+        return  d;
+    }
+
+    @Override
+    public String resolverOperaciones(String operacion) throws Exception {
+        try {
+            ScriptEngineManager opera = new ScriptEngineManager();
+            ScriptEngine engine = opera.getEngineByName("JavaScript");
+             return engine.eval(operacion).toString();
+        } catch (Exception e) {
+            System.out.println("Error casteando operacion"+ e.getMessage());
+        }
+        return operacion;
+
+    }
+    
+}
