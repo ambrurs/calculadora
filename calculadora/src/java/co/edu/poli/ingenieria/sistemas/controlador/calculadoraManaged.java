73e1ac7de5253d91a6f7b23dec6b06318221f65b
andres <andres@andres-PC>
2016-09-25 23:05:20 -0500

    Ajuste en el controlador

--- calculadora/src/java/co/edu/poli/ingenieria/sistemas/controlador/calculadoraManaged.java
+++ calculadora/src/java/co/edu/poli/ingenieria/sistemas/controlador/calculadoraManaged.java
@@ -8,6 +8,7 @@ package co.edu.poli.ingenieria.sistemas.controlador;
 
 import co.edu.poli.ingenieria.sistemas.entidad.BotonesNumeros;
 import co.edu.poli.ingenieria.sistemas.entidad.BotonesOperaciones;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -28,21 +29,71 @@ public class calculadoraManaged {
      * Variable
      */
     public String display;
+    public String operacionEnMemoria;
+    public List<String> operacionesList;
     public int numero;
 
     public calculadoraManaged() {
         display = "0";
     }
+    
+    public void oprimirBoton(String boton){
+        /**Dado el caso que sea un numero*/
+        impirmirDisplay(boton);
+    }
 
     public void impirmirDisplay(String boton) {
        
-        if (display.isEmpty()) {
+        if (display.isEmpty() || display.equals("0")) {
             display = boton;
         } else { 
             display += boton;
         }
 
+        operacionesList.add(boton);
     }
+    
+    public void determinarOperación(){
+        int index = 0;
+        int opera = 0;
+        for(String boton : operacionesList){
+            
+            switch(boton){
+                case BotonesOperaciones.SUMA :
+                    /**Suma*/
+                    opera = Integer.parseInt(operacionesList.get(index-1).toString()) 
+                            + Integer.parseInt(operacionesList.get(index+1).toString()) ;
+                    break;
+                case BotonesOperaciones.RESTA:
+                     opera = Integer.parseInt(operacionesList.get(index-1).toString()) 
+                            / Integer.parseInt(operacionesList.get(index+1).toString()) ;
+                    break;
+                case BotonesOperaciones.MULTIPLICACION:
+                    opera = Integer.parseInt(operacionesList.get(index-1).toString()) 
+                            * Integer.parseInt(operacionesList.get(index+1).toString()) ;
+                    break;
+
+                case BotonesOperaciones.DIVISION:
+                    opera = Integer.parseInt(operacionesList.get(index-1).toString()) 
+                            / Integer.parseInt(operacionesList.get(index+1).toString()) ;
+                    break;
+
+                case BotonesOperaciones.RAIZ:
+                     opera = Integer.parseInt(operacionesList.get(index-1).toString()) ;
+                     break;
+
+                case BotonesOperaciones.PORCENTAJE:
+                    opera = Integer.parseInt(operacionesList.get(index-1).toString());
+                    break;
+                case BotonesOperaciones.PUNTO:
+                    operacionesList.set(index-1, operacionesList.get(index-1).toString() + boton );
+                    break;
+            }
+            index++;
+        }
+    }
+    
+    
 
     public String getDisplay() {
         return display;
@@ -52,27 +103,21 @@ public class calculadoraManaged {
         this.display = display;
     }
 
-    public int getNumero() {
-        return numero;
+    public String getOperacionEnMemoria() {
+        return operacionEnMemoria;
     }
 
-    public void setNumero(int numero) {
-        this.numero = botonesNumeros.valor;
+    public void setOperacionEnMemoria(String operacionEnMemoria) {
+        this.operacionEnMemoria = operacionEnMemoria;
     }
 
-    public BotonesOperaciones getBotonesOperaciones() {
-        return botonesOperaciones;
+    public List<String> getOperacionesList() {
+        return operacionesList;
     }
 
-    public void setBotonesOperaciones(BotonesOperaciones botonesOperaciones) {
-        this.botonesOperaciones = botonesOperaciones;
+    public void setOperacionesList(List<String> operacionesList) {
+        this.operacionesList = operacionesList;
     }
-
-    public BotonesNumeros getBotonesNumeros() {
-        return botonesNumeros;
-    }
-
-    public void setBotonesNumeros(BotonesNumeros botonesNumeros) {
-        this.botonesNumeros = botonesNumeros;
-    }
+    
+    
 }
