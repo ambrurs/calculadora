4df6b68e0f4ee78653c0e31a1f230e6c5fa010b1
andres <andres@andres-PC>
2016-09-25 23:04:13 -0500

    Ajuste en el controlador

--- calculadora/src/java/co/edu/poli/ingenieria/sistemas/entidad/BotonesOperaciones.java
+++ calculadora/src/java/co/edu/poli/ingenieria/sistemas/entidad/BotonesOperaciones.java
@@ -11,41 +11,13 @@ package co.edu.poli.ingenieria.sistemas.entidad;
  * @author andres
  */
 
-public enum BotonesOperaciones {
+public class BotonesOperaciones {
     /**Operaciones actuales*/
-    SUMA("+",1),
-    RESTA("-",2),
-    MULTIPLICACION("*",3),
-    DIVISION("/",4),
-    RAIZ("&#8730;",5),
-    PORCENTAJE("%",6);
-    
-    
-    public String nombre;
-    public int id;
-    /**Se sobreescribe el constructor */
-    private BotonesOperaciones(String nombre , int id){
-        this.nombre = nombre;
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    
-    
-    
+    public final static String SUMA = "+";
+    public final static String RESTA = "-";
+    public final static String MULTIPLICACION = "*";
+    public final static String DIVISION="/";
+    public final static String RAIZ = "&#8730;";
+    public final static String PORCENTAJE ="%";
+    public final static  String PUNTO = ".";
 }
